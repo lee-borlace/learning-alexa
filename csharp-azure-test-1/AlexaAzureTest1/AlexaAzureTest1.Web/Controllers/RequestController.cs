9c41263aaa76982485e86eaacdb06d3ea70e7182
Lee Borlace <lee.borlace@gmail.com>
2018-05-08 17:38:32 +0800

    Build up initial content.

---
+++ csharp-azure-test-1/AlexaAzureTest1/AlexaAzureTest1.Web/Controllers/RequestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexaAzureTest1.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Request")]
+    public class RequestController : Controller
+    {
+        // POST api/request
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]SkillRequest request)
+        {
+            var speech = new Alexa.NET.Response.SsmlOutputSpeech();
+            speech.Ssml = "<speak>Hello World!</speak>";
+            var finalResponse = ResponseBuilder.Tell(speech);
+            return Ok(finalResponse);
+
+            // check what type of a request it is like an IntentRequest or a LaunchRequest
+            //var requestType = request.GetRequestType();
+
+            //if (requestType == typeof(IntentRequest))
+            //{
+            //    var speech = new Alexa.NET.Response.SsmlOutputSpeech();
+            //    speech.Ssml = "<speak>Hello World!</speak>";
+            //    var finalResponse = ResponseBuilder.Tell(speech);
+            //    return Ok(finalResponse);
+            //}
+            //else if (requestType == typeof(Alexa.NET.Request.Type.LaunchRequest))
+            //{
+            //    ResponseBuilder.
+            //}
+            //else if (requestType == typeof(AudioPlayerRequest))
+            //{
+            //    // do some audio response stuff
+            //}
+
+
+        }
+    }
+}
---
+++ csharp-azure-test-1/AlexaAzureTest1/AlexaAzureTest1.Web/readme.md
@@ -0,0 +1,7 @@
+﻿# Running Locally
+1. Install ngrok.
+2. Run this project.
+3. Run ```ngrok http -bind-tls=true -host-header=rewrite 61979```. 
+4. The above will give you a URL in the ngrok domain e.g. ```https://eb6554ac.ngrok.io```. This will change every time you run that step!
+5. Plug that URL into the endpoint section of the Alexa app with ```/api/request``` on the end e.g. ```https://eb6554ac.ngrok.io/api/request```.
+6. Talk to the Alexa app!
